db4c5c615180eaa815308fe0bfbf47d18b913838
Matt Le <ml9951@rit.edu>
2016-09-07 17:28:18 -0400

    Adding testing framework (not much there at the moment...)

---
+++ test/index.js
@@ -0,0 +1,3 @@
+
+
+require('./specs/Explore/PolicyMenu');
---
+++ test/specs/Explore/PolicyMenu.jsx
@@ -0,0 +1,16 @@
+// Test Cases for PolicyMenu Component
+
+import React from 'react';
+import {expect} from 'chai';
+import {mount, render} from 'enzyme';
+import PolicyMenu from 'sociome/components/PolicyMenu';
+
+describe('<PolicyMenu/>', () => {
+	it('Mount component', () => {
+		const wrapper = render(
+			<PolicyMenu/>
+		)	
+	}) 
+	expect(true)
+})
+
