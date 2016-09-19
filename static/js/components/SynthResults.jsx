bfbb96e67f5326f1c135c078e34958b20a67cc4d
Matt Le <ml9951@rit.edu>
2016-09-19 16:18:06 -0400

    Adding bar plot back in to SynthResults component

--- static/js/components/SynthResults.jsx
+++ static/js/components/SynthResults.jsx
@@ -23,28 +23,43 @@ export default class SynthResults extends React.Component{
 			}
 		}	
 
+		var barData = []
+		for(var i = 0; i < this.props.states.length; i++){
+			var obj = {name : this.props.states[i].label, synth : this.props.results ? this.props.results.weights[i] : 0.0}
+			barData.push(obj);
+		}
+
 		return(
-			<div style={{width : '70%', height : '100%', paddingTop : '10%'}}>
-				<ResponsiveContainer  width='90%' height='40%'>
-					<LineChart data={data}>
-						<Line type="monotone" dataKey="treated" stroke="#8884d8" activeDot={{r:8}}/>
-						<Line type="monotone" dataKey="synthetic" stroke="#82ca9d"/>
-						<Tooltip/>
-						<Legend/>
-						<XAxis dataKey="name" label="Time"/>
-						<YAxis/>
-						<CartesianGrid strokeDasharray="3 3"/>
-					</LineChart>
-				</ResponsiveContainer>
+			<div style={{width : '100%', height : '70%', paddingTop : '10%'}}>
+				<div style={{width : '100%', height : '50%'}}>
+					<ResponsiveContainer  width='90%' height='100%'>
+						<LineChart data={data}>
+							<Line type="monotone" dataKey="treated" stroke="#8884d8" activeDot={{r:8}}/>
+							<Line type="monotone" dataKey="synthetic" stroke="#82ca9d" activeDot={{r:8}}/>
+							<Tooltip/>
+							<Legend/>
+							<XAxis dataKey="name" label="Time"/>
+							<YAxis/>
+							<CartesianGrid strokeDasharray="3 3"/>
+						</LineChart>
+					</ResponsiveContainer>
+				</div>
+				<div style={{width : '100%', height : '50%'}}>
+					<ResponsiveContainer  width='90%' height='100%'>
+						<BarChart data={barData}>
+							<Bar key='name' dataKey='synth' fill='#8884d8'/>
+							<Tooltip/>
+							<XAxis dataKey="name"/>
+							<YAxis/>
+							<CartesianGrid strokeDasharray="3 3"/>
+						</BarChart>
+					</ResponsiveContainer>
+				</div>
 			</div>
 		)
 
 		/*
-		var data = []
-		for(var i = 0; i < this.props.states.length; i++){
-			var obj = {name : this.props.states[i].label, synth : this.props.results ? this.props.results[i] : 0.0}
-			data.push(obj);
-		}
+		
 
 		return(
 			<div style={{width : '70%', height : '100%', paddingTop : '10%'}}>
