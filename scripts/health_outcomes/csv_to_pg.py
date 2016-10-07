9b56d6bdd8c9e69da40903e2f053b770b5369691
Matt Le <ml9951@rit.edu>
2016-10-07 16:46:05 -0400

    Leave health outcomes data in a single table
    Creating an index on the measurename leads to the same performance
    but is much cleaner

--- scripts/health_outcomes/csv_to_pg.py
+++ scripts/health_outcomes/csv_to_pg.py
@@ -2,70 +2,67 @@
 
 import csv, pdb, re, glob
 import download_file
+import pandas, sys
 
 if len(glob.glob('data/*.csv')) == 0:
 	download_file.downloadAll()
 
 
 schema = [
-	'int', 
-	'int', 
-	'int', 
-	'text',
-	'text', 
-	'real', 
-	'real', 
-	'real',
-	'real', 
-	'real',
-	'int', 
-	'bool', 
-	'bool'
+	'text', #yearspan
+	'text', #measurename
+    'int', #statecode
+    'int', #countycode
+	'text', #county
+    'text', #state
+	'real', #numerator
+	'real', #denominator
+	'real', #rawvalue
+	'real', #cilow
+	'real', #cihigh
+	'int', #measureid
+	'bool', #differflag
+	'bool',#trendbreak
+    'int'  #year column
 ]
-
-def unicode_csv_reader(utf8_data, dialect=csv.excel, **kwargs):
-    csv_reader = csv.reader(utf8_data, dialect=dialect, **kwargs)
-    for row in csv_reader:
-        yield [unicode(cell, 'latin-1') for cell in row]
 
 scriptStream = open('script.txt', 'w')
 
+
+def castToInt(v, field):
+    v[field] = v[field] if pandas.isnull(v[field]) else int(v[field])
+
 def formatVal(v):
-	if v == '':
-		return 'NULL'
-	return v.replace(',', '').encode('utf-8')
+    v['measureid'] = v['measureid'] if pandas.isnull(v['measureid']) else int(v['measureid']) 
+    castToInt(v, 'measureid')
+    castToInt(v, 'differflag')
+    castToInt(v, 'trendbreak')
+    scriptStream.write('\t'.join([str(v) for v in v.values]) + '\n')
 
-with open('data/2016CHR_CSV_Trend_Data.csv', 'rb') as csvfile:
-    reader = unicode_csv_reader(csvfile)
-    headers = reader.next()
-    headers[0] = 'year' #change this from yearspan
-    headers.remove('measurename')
-    data = {}
-    for row in reader:
-    	row[1] = re.sub('[ |-]+', '_', row[1].lower())
+data = pandas.read_csv('data/2016CHR_CSV_Trend_Data.csv')
 
-    	# Empty measurename for some reason?
-    	if row[1] == '':
-    		continue;
+def midYear(ystr):
+    if type(ystr) is str:
+        rangeY = ystr.split('-')
+        return str((int(rangeY[0]) + int(rangeY[1])) / 2) if len(rangeY) > 1 else ystr
+    else:
+        return ystr
 
-    	if '-' in row[0]:
-    		years = row[0].split('-')
-    		year = (int(years[0]) + int(years[1])) / 2
-    		row[0] = str(year)
-    	s = row[0] + '\t' + '\t'.join(map(formatVal, row[2:]))
-    	if row[1] in data:
-			data[row[1]].append(s)
-    	else:
-    		data[row[1]] = [s]
+data['year'] = data['yearspan'].apply(midYear)
+data['rawvalue'] = data['rawvalue'].apply(lambda x: x.replace(',', '') if type(x) is str else x)
+data['cilow'] = data['cilow'].apply(lambda x: x.replace(',', '') if type(x) is str else x)
+data['cihigh'] = data['cihigh'].apply(lambda x: x.replace(',', '') if type(x) is str else x)
 
-    for key in data:
-    	scriptStream.write('DROP TABLE IF EXISTS %s;\n' % key)
-    	scriptStream.write('CREATE TABLE %s (%s);\n' % (key, ','.join(map(lambda x: '%s %s' % (x[0], x[1]), zip(headers, schema)))))
-    	scriptStream.write('COPY %s (%s) FROM stdin WITH NULL AS \'NULL\';\n' % (key, ','.join(headers)))
-    	keyData = data[key]
-    	try:
-    		scriptStream.write('\n'.join(keyData))
-    	except Exception as e:
-    		pdb.set_trace()
-    	scriptStream.write('\n\\.\n')
 
+schema = ','.join('%s %s' % (x[0], x[1]) for x in zip(data.columns, schema))
+
+scriptStream.write('SET CLIENT_ENCODING TO LATIN1;\n')
+scriptStream.write('DROP TABLE IF EXISTS health_outcomes;\n')
+scriptStream.write('CREATE TABLE health_outcomes (%s);\n' % schema)
+scriptStream.write('COPY health_outcomes (%s) FROM stdin WITH NULL AS \'nan\';\n' % (','.join(data.columns)))
+
+data.apply(formatVal, axis=1)
+
+scriptStream.write('\\.\n')
+
+scriptStream.write('CREATE INDEX measurename ON health_outcomes ((lower(measurename)));\n')
