440c38e9b4d3679ed6b34c4176254caa5c270c0b
Matt Le <ml9951@rit.edu>
2017-04-28 11:18:47 -0400

    Updating policy script to create policy_metadata table

--- scripts/policy/join_policy.py
+++ scripts/policy/join_policy.py
@@ -6,6 +6,8 @@ import psycopg2, glob, os, pdb
 conn = psycopg2.connect(dbname='sociome')
 cur = conn.cursor()
 
+cur.execute('DROP TABLE IF EXISTS policy, policy_metadata;')
+
 query = 'CREATE TABLE policy AS '
 first = True
 count = 0
@@ -17,6 +19,19 @@ joinCols = {'state', 'year', 'statecode', 'countycode'}
 duplicates = {}
 
 seen = set()
+
+cur.execute('CREATE TABLE policy_metadata(varaible_name text, variable_code text, source_name text)')
+
+def getCols(table):
+    cur.execute("""
+        SELECT column_name
+            FROM information_schema.columns
+            WHERE table_schema = 'public'
+              AND table_name   = '%s'
+    """ % table)
+    res = map(lambda x: '"%s"' % x[0].replace('"', '""'), cur.fetchall()[0:3])
+    print(res)
+    return res
 
 for file in glob.glob('policy/*.xls*'):
     table = os.path.splitext(os.path.basename(file))[0]
@@ -48,12 +63,16 @@ for file in glob.glob('policy/*.xls*'):
     else:
         query += ' FULL OUTER JOIN %s USING (state, year, statecode, countycode) ' % table
     first = False
+    print(table)
+    metatable = table + '_metadata'
+    cur.execute('INSERT INTO policy_metadata SELECT %s FROM %s' % (','.join(getCols(metatable)), metatable))
 
 for key in duplicates:
     tables = duplicates[key]
     for table in tables:
         cur.execute('ALTER TABLE %s DROP COLUMN %s;' % (table, key))
 
+print(query)
 
 cur.execute(query)
 conn.commit()
